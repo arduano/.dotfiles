c35d11332f44f5c4a1fba45bc2d8a92b89381de5
arduano <leonid.shchurov@gmail.com>
2024-03-05 22:01:44 +1100

    fix

--- flake.nix
+++ flake.nix
@@ -38,72 +38,21 @@
   outputs = { self, nixpkgs, nixos-hardware, vscode-server, flake-utils, ... }@inputs:
     with inputs;
     let
-      lib = nixpkgs.lib;
-
-      commonSystemModules = [
-        nix-flatpak.nixosModules.nix-flatpak
-        inputs.home-manager.nixosModules.home-manager
-        (import ./share/nixModules)
-        (import ./share/overlayModule.nix)
-
-        # TODO: https://github.com/wamserma/flake-programs-sqlite
-        flake-programs-sqlite.nixosModules.programs-sqlite
-      ];
-
-      commonHomeModules = [
-        nix-flatpak.homeManagerModules.nix-flatpak
-        vscode-server.homeModules.default
-        (import ./share/homeModules)
-        (import ./share/overlayModule.nix)
-      ];
-
-      makeSystem = { systemModules ? [ ], homeModules ? [ ], extraArgs ? { }, system ? "x86_64-linux" }:
-        lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = extraArgs // {
-            inherit inputs;
-          };
-          modules = commonSystemModules ++ systemModules ++ [{
-            home-manager.users.arduano = {
-              imports = commonHomeModules ++ homeModules;
-            };
-            home-manager.extraSpecialArgs = {
-              inherit inputs;
-            };
-          }];
-        };
-
-      # systems = {
-      #   nixosConfigurations.main-pc = makeSystem {
-      #     systemModules = [
-      #       ./main-pc/system
-      #       nixos-hardware.nixosModules.common-pc
-      #       nixos-hardware.nixosModules.common-pc-ssd
-      #       nixos-hardware.nixosModules.common-pc-hdd
-      #       nixos-hardware.nixosModules.common-cpu-amd
-      #     ];
-      #     homeModules = [
-      #       ./main-pc/home
-      #       plasma-manager.homeManagerModules.plasma-manager
-      #     ];
-      #   };
-      # };
-
       systems = (import ./systems.nix) inputs;
 
-          packages= flake-utils. lib. eachDefaultSystem
-          ( system:
-        let
-          pkgs= import nixpkgs{
-        inherit system;
-        overlays = [ (import ./share/overlay.nix) ];
-      };
-    in
-    {
-      packages = pkgs.arduano;
-    }
-    );
+      packages = flake-utils.lib.eachDefaultSystem
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ (import ./share/overlay.nix) ];
+            };
+          in
+          {
+            packages = pkgs.arduano;
+          }
+        );
 
-  in
-  packages // systems;
-  }
+    in
+    packages // systems;
+}

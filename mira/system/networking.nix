9b27e96846b35401dac863ca46eaeaca6532084f
arduano <leonid.shchurov@gmail.com>
2024-03-28 15:49:44 +1100

    Updated networking config for mira

--- mira/system/networking.nix
+++ mira/system/networking.nix
@@ -5,7 +5,7 @@
     nameservers = [
       "8.8.8.8"
     ];
-    defaultGateway = "170.64.192.1";
+    defaultGateway = "170.64.224.1";
     defaultGateway6 = {
       address = "2400:6180:10:200::1";
       interface = "eth0";
@@ -15,14 +15,14 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          { address = "170.64.216.104"; prefixLength = 19; }
+          { address = "170.64.226.96"; prefixLength = 20; }
           { address = "10.49.0.5"; prefixLength = 16; }
         ];
         ipv6.addresses = [
-          { address = "2400:6180:10:200::af:0"; prefixLength = 64; }
-          { address = "fe80::689c:a7ff:fe73:1cfb"; prefixLength = 64; }
+          { address = "2400:6180:10:200::59:0"; prefixLength = 64; }
+          { address = "fe80::3c2b:abff:feaa:eab"; prefixLength = 64; }
         ];
-        ipv4.routes = [{ address = "170.64.192.1"; prefixLength = 32; }];
+        ipv4.routes = [{ address = "170.64.224.1"; prefixLength = 32; }];
         ipv6.routes = [{ address = "2400:6180:10:200::1"; prefixLength = 128; }];
       };
       eth1 = {
@@ -30,13 +30,13 @@
           { address = "10.126.0.2"; prefixLength = 20; }
         ];
         ipv6.addresses = [
-          { address = "fe80::cc7e:98ff:fee6:9631"; prefixLength = 64; }
+          { address = "fe80::1017:ceff:fee8:411c"; prefixLength = 64; }
         ];
       };
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="6a:9c:a7:73:1c:fb", NAME="eth0"
-    ATTR{address}=="ce:7e:98:e6:96:31", NAME="eth1"
+    ATTR{address}=="3e:2b:ab:aa:0e:ab", NAME="eth0"
+    ATTR{address}=="12:17:ce:e8:41:1c", NAME="eth1"
   '';
 }

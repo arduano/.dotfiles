4d4273d066aa2301f6fb929425a22f80fca5a47e
arduano <leonid.shchurov@gmail.com>
2024-04-11 20:16:58 +1000

    fixed sunshine

--- share/pkgs/sunshine/default.nix
+++ share/pkgs/sunshine/default.nix
@@ -1,8 +1,12 @@
-{ sunshine
+it { sunshine
 , ...
 }:
 sunshine.overrideAttrs (oldAttrs: rec {
   patches = oldAttrs.patches ++ [
     ./screens.patch
+  ];
+
+  cmakeFlags = oldAttrs.cmakeFlags ++ [
+    "-DSUNSHINE_ENABLE_DRM=OFF"
   ];
 })

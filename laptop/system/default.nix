9cd2f9893644a2141646443a787d50612aea60b7
arduano <leonid.shchurov@gmail.com>
2024-06-03 15:01:10 +1000

    Revert kernel

--- laptop/system/default.nix
+++ laptop/system/default.nix
@@ -79,8 +79,7 @@
     };
   };
 
-  # boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelPackages = pkgs.linuxPackages_6_8;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   # boot.kernelPackages = pkgs.linuxPackages_testing;
 
   environment.systemPackages = with pkgs;

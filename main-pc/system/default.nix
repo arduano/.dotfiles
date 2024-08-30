1fc74eb0edbbe455eba61bb6474a8d6ca542d03c
arduano <leonid.shchurov@gmail.com>
2024-08-31 08:43:54 +1000

    Updates

--- main-pc/system/default.nix
+++ main-pc/system/default.nix
@@ -123,7 +123,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.beta;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
   };
 
   # This value determines the NixOS release from which the default

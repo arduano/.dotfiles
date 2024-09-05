79d1f1a10e227abbb381faec6ee6e0dc25af49cd
arduano <leonid.shchurov@gmail.com>
2024-09-05 21:14:38 +1000

    Update

--- share/overlay.nix
+++ share/overlay.nix
@@ -1,3 +1,11 @@
 final: prev: {
   arduano = final.callPackage ./pkgs/default.nix { };
+
+  # Disable cuda in certain packages for better build cache
+  opencv = prev.opencv.override {
+    enableCuda = false;
+  };
+  opencv4 = prev.opencv4.override {
+    enableCuda = false;
+  };
 }

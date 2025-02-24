f6090eb6d18a86c1763f65273f0546c494410f96
arduano <leonid.shchurov@gmail.com>
2025-02-24 17:22:59 +1100

    networking kernel modules

--- home-nas/system/hardware-configuration.nix
+++ home-nas/system/hardware-configuration.nix
@@ -5,7 +5,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
+  boot.kernelModules = [ "iptable_nat" "iptable_filter" ];
   boot.extraModulePackages = [ ];
   hardware.enableRedistributableFirmware = true;
 

24473a405f858c34838148f5459e6a2e3925f0a8
arduano <leonid.shchurov@gmail.com>
2026-02-21 14:45:52 +1100

    home-nas: add gcloud+jq prerequisites for Gmail/OpenClaw setup

--- home-nas/home/default.nix
+++ home-nas/home/default.nix
@@ -18,6 +18,9 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    # Gmail/OpenClaw tooling prerequisites
+    google-cloud-sdk
+    jq
   ];
 
 

d1c39941cc5123afa7520daa1ea380f84ae655a4
arduano <leonid.shchurov@gmail.com>
2026-04-19 20:22:48 +1000

    Use bcachefs UUID root on home-nas

--- home-nas/system/hardware-configuration.nix
+++ home-nas/system/hardware-configuration.nix
@@ -7,14 +7,21 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-  boot.initrd.systemd.enable = false;
+  boot.initrd.systemd.enable = true;
   hardware.enableRedistributableFirmware = true;
 
   fileSystems."/" =
     {
-      device = "/dev/sda:/dev/sdb:/dev/sdc:/dev/sdd:/dev/nvme0n1p2";
+      device = "UUID=aa5beb5c-ede2-4bf5-a042-f56bb0a7adc2";
       fsType = "bcachefs";
-      options = [ "degraded" ];
+      options = [
+        "degraded"
+        "x-systemd.requires=/dev/disk/by-id/ata-ST8000VN002-2ZM188_WPV2NDW6"
+        "x-systemd.requires=/dev/disk/by-id/ata-WDC_WD80EFPX-68C4ZN0_WD-RD1B44VD"
+        "x-systemd.requires=/dev/disk/by-id/ata-WDC_WD80EFPX-68C4ZN0_WD-RD1DNDWD"
+        "x-systemd.requires=/dev/disk/by-id/ata-ST8000VN002-2ZM188_WPV2NBA6"
+        "x-systemd.requires=/dev/disk/by-id/nvme-eui.0025385581b21585-part2"
+      ];
     };
 
   fileSystems."/boot" =

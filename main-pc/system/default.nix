115e594cdfa2db722f089522d49950ecd977a517
arduano <leonid.shchurov@gmail.com>
2026-05-16 15:20:40 +1000

    Remove retired hosts and orphaned config

--- main-pc/system/default.nix
+++ main-pc/system/default.nix
@@ -4,7 +4,6 @@
   imports = [
     ./arduano.nix
     ./hardware-configuration.nix
-    ./sdk
   ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
--- systems.nix
+++ systems.nix
@@ -71,13 +71,4 @@ in
       ./home-nas/home
     ];
   };
-
-  nixosConfigurations.mira = makeSystem {
-    systemModules = [
-      ./mira/system
-    ];
-    homeModules = [
-      ./mira/home
-    ];
-  };
 }
